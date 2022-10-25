2bc306ff6affa7055bddf46d11cecf29c685a6f9
gowtham <gowthamarajan97@gmail.com>
2022-10-25 16:36:09 +0530

    add: aws provider

--- terraform/ecr/ecr.tf
+++ terraform/ecr/ecr.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region     = var.region
+}
+
 resource "aws_ecr_repository" "opstree_ecr_repo" {
   count  = 2
   name   = element(var.ecr_repo_name, count.index)
--- terraform/ecr/variables.tf
+++ terraform/ecr/variables.tf
@@ -1,3 +1,9 @@
+variable "region" {
+  type          = string
+  default       = "ap-south-1"
+  description   = "AWS VPC region"
+}
+
 variable "ecr_repo_name" {
   type          = list(string)
   default       = ["webapp", "mysql"]
