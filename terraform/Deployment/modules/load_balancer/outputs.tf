11882d655abe3d21c5e283250f9e70920b2e397f
gowtham <gowthamarajan97@gmail.com>
2022-10-25 16:07:05 +0530

    remove: terraform/Deployment

--- terraform/Deployment/modules/load_balancer/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "public_alb_dns" {
-    value = aws_lb.opstree_public_alb.dns_name
-}
--- terraform/Deployment/support_files/ecs_instance_user_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo yum install mysql -y
-echo 'ECS_CLUSTER=my-ecs-cluster' >> /etc/ecs/ecs.config
-echo 'ECS_DISABLE_PRIVILEGED=true' >> /etc/ecs/ecs.config
